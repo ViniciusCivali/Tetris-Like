7965328aff12cc3bd3c0128e40f4d648a990eeaf
Vinicius Civali <viniciuscivali@gmail.com>
2021-10-10 15:16:29 -0300

    Starting to do the bounds of movement

--- Tetris-Like/Assets/Scripts/Tetromino.cs
+++ Tetris-Like/Assets/Scripts/Tetromino.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    //It marks the start of the period of time passed
+    private float startTimePeriod = 0;
+    //One second
+    private float timePeriodToFall = 1;
+    public float fallSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,104 @@ public class Tetromino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckTime();
+        CheckUserInput();
     }
+
+    void CheckUserInput ()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MoveAround();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveDown();
+        }
+    }
+
+    void CheckTime()
+    {
+ 
+        if ((Time.time - startTimePeriod) >= timePeriodToFall)
+        {
+            ConstantDownwardMove();
+            startTimePeriod = Time.time;
+        }
+    }
+
+    void ConstantDownwardMove()
+    {
+        transform.position += new Vector3(0, -fallSpeed, 0);
+
+        if (!CheckIsValidPosition())
+        {
+            transform.position += new Vector3(0, fallSpeed, 0);
+        }
+    }
+
+    void MoveRight()
+    {
+
+        transform.position += new Vector3(1, 0, 0);
+
+
+        if (!CheckIsValidPosition())
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+
+    void MoveLeft()
+    {
+        transform.position += new Vector3(-1, 0, 0);
+
+        if (!CheckIsValidPosition())
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+    void MoveAround()
+    {
+        transform.Rotate(0, 0, 90);
+
+        if (!CheckIsValidPosition())
+        {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+
+    void MoveDown()
+    {
+        transform.position += new Vector3(0, -1, 0);
+
+        if (!CheckIsValidPosition())
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+
+            if (FindObjectOfType<Game>().CheckIsInsideGrid(pos) == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
